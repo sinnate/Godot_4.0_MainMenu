393dab19c510fd9b960f739ec112dc366d0c16e8
Sinnate Mohammed Amine <contact@sinnate.dev>
2023-03-05 17:15:28 +0100

    setup

---
+++ src/menu_controller.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+
+func _process(delta):
+	pass
+
+
+func _on_start_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_option_button_toggled(button_pressed):
+	pass # Replace with function body.
+
+
+func _on_exit_button_pressed():
+	get_tree().quit()
